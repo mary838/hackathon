720f54f34f135baaa40f4041c68a54d151f67c05
mary <thoeun.mary@institute.pse.ngo>
2025-01-24 13:50:56 +0700

    add js

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
-
-const display= document.getElementById("result");
-function appendNumber(input){
-    display.value += input;
+const display = document.getElementById("result");
+function appendNumber(input) {
+  display.value += input;
 }
 
-function clearResult(){
-    display.value= "";
+function clearResult() {
+  display.value = "";
 }
-
-function calculate(){
-    display.value=eval(display.value)
-    
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = "Error";
+  }
 }
-
-function backspace(){
-    display.value=display.value.slice(0,-1)
+function backspace() {
+  display.v;
 }
-
-function square(){
-    display.value=display.value*display.value
+function square() {
+  display.value = display.value * display.value;
 }
-function trangle(){
-    display.value=display.value*display.value*display.value;
+function persiontage() {
+  display.value = display.value / 100;
 }
-function persiontage(){
-    display.value=display.value/100
+function backspace() {
+  display.value = display.value.slice(0, -1);
 }
